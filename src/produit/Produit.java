35855ad3d9519669d654e7e54bedef2648835593
VRD3647A <VRD3647A@u3-202-18.ens.fsi>
2023-11-08 11:55:26 +0100

    Debut Tp4

---
+++ src/produit/Produit.java
@@ -0,0 +1,36 @@
+package produit;
+
+public abstract class  Produit {
+	public String nom;
+	public String unite;
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	
+	public class Poisson extends Produit{
+		super(nom);
+		private String chasseur;
+		
+		
+		
+  
+	}
+	
+	public class Sanglier extends Produit{
+		super(nom);
+		private String date;
+		
+		
+		
+	  
+	}
+	      
+	
+	public void DescriptionProduit() {
+		
+	
+}
+	
+}}
